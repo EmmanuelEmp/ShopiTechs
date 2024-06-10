a0ee1702eacbd6000c588d00a6e33285fc47ca37
EmmanuelEmp <emmyochogwu@gmail.com>
2024-06-09 22:30:42 -0400

    deployment fixed it

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FaCaretDown, FaCaretUp } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { useLogoutMutation } from '../slices/userApiSlice';
 import { logout } from '../slices/userSlice';
-import { loadCartItems, clearCartItems } from '../slices/cartSlice'; // Update import statement
+import { loadCartItems } from '../slices/cartSlice';
 import './css/Header.css';
 
 const Header = () => {
@@ -35,7 +35,6 @@ const Header = () => {
         try {
             await logoutApi().unwrap();
             dispatch(logout());
-            // dispatch(clearCartItems()); // Clear cart items on logout
             navigate('/login');
             toast.success('Logged Out Successfully');
         } catch (error) {
@@ -43,72 +42,68 @@ const Header = () => {
         }
     };
 
-    const renderProfileButton = () => {
-        return (
-            <>
-                <button
-                    onClick={() => setIsProfileMenuOpen(!isProfileMenuOpen)}
-                    className="bg flex items-center"
-                >
-                    <FiUser className="mr-1" />
-                    {userInfo?.name}
-                    {isProfileMenuOpen ? <FaCaretUp /> : <FaCaretDown />}
-                </button>
-                <ul
-                    className={`absolute ${isProfileMenuOpen ? 'block' : 'hidden'
-                        } bg-white p-2 mt-2 space-y-2 bg border rounded-md`}
-                >
-                    <li>
-                        <Link to="/profile">
-                            <FiUser className="mr-1" />
-                            Profile
-                        </Link>
-                    </li>
-                    <li>
-                        <Link onClick={handleLogout}>
-                            <FiLogOut className="mr-1" />
-                            Logout
-                        </Link>
-                    </li>
-                </ul>
-            </>
-        );
-    };
+    const renderProfileButton = () => (
+        <>
+            <button
+                onClick={() => setIsProfileMenuOpen(!isProfileMenuOpen)}
+                className="bg flex items-center"
+            >
+                <FiUser className="mr-1" />
+                {userInfo?.name}
+                {isProfileMenuOpen ? <FaCaretUp /> : <FaCaretDown />}
+            </button>
+            <ul
+                className={`absolute ${isProfileMenuOpen ? 'block' : 'hidden'
+                    } bg-white p-2 mt-2 space-y-2 bg border rounded-md`}
+            >
+                <li>
+                    <Link to="/profile">
+                        <FiUser className="mr-1" />
+                        Profile
+                    </Link>
+                </li>
+                <li>
+                    <Link onClick={handleLogout}>
+                        <FiLogOut className="mr-1" />
+                        Logout
+                    </Link>
+                </li>
+            </ul>
+        </>
+    );
 
-    const renderAdminButton = () => {
-        return (
-            <>
-                <button
-                    onClick={() => setIsAdminMenuOpen(!isAdminMenuOpen)}
-                    className="bg flex items-center admin"
-                >
-                    <FiUser className="mr-1" />
-                    Admin
-                    {isAdminMenuOpen ? <FaCaretUp /> : <FaCaretDown />}
-                </button>
-                <ul
-                    className={`absolute ${isAdminMenuOpen ? 'block' : 'hidden'
-                        } bg-white p-2 mt-2 space-y-2 bg border rounded-md`}
-                >
-                    <li>
-                        <Link to="/admin/users">
-                            Users
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/products">
-                            Products
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/orders">
-                            Orders
-                        </Link>
-                    </li>
-                </ul>
-            </>
-        );
-    };
+    const renderAdminButton = () => (
+        <>
+            <button
+                onClick={() => setIsAdminMenuOpen(!isAdminMenuOpen)}
+                className="bg flex items-center admin"
+            >
+                <FiUser className="mr-1" />
+                Admin
+                {isAdminMenuOpen ? <FaCaretUp /> : <FaCaretDown />}
+            </button>
+            <ul
+                className={`absolute ${isAdminMenuOpen ? 'block' : 'hidden'
+                    } bg-white p-2 mt-2 space-y-2 bg border rounded-md`}
+            >
+                <li>
+                    <Link to="/admin/users">
+                        Users
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/admin/products">
+                        Products
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/admin/orders">
+                        Orders
+                    </Link>
+                </li>
+            </ul>
+        </>
+    );
 
     const renderSignInButton = () => (
         <Link className="flex items-center" to="/login">
@@ -130,31 +125,37 @@ const Header = () => {
     return (
         <nav className="navbar p-4">
             <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                    <Link to="/" className="text-white text-2xl nav-logo font-extrabold">
-                        <p className="font-extrabold">Shopi<span>Tech.</span></p>
-                    </Link>
+                <Link to="/" className="text-white text-2xl nav-logo font-extrabold">
+                    <p className="font-extrabold">Shopi<span>Tech.</span></p>
+                </Link>
+                <div className="sm:hidden flex items-center">
+                    <button
+                        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+                        className="bg text-xl focus:outline-none"
+                    >
+                        ☰
+                    </button>
+                </div>
+                <div className="hidden sm:flex items-center space-x-4">
                     <div className="flex items-center justify-between search">
                         <input
                             type="text"
                             placeholder="Search"
-                            className="ml-4 p-2 rounded-md border hidden sm:block"
+                            className="ml-4 p-2 rounded-md border"
                             value={keyword}
                             onChange={(e) => setKeyword(e.target.value)}
                         />
                         <button
-                            className="bg-s text-white py-2 px-4 rounded-md hidden sm:block ml-2"
+                            className="bg-s text-white py-2 px-4 rounded-md ml-2"
                             onClick={handleSearch}
                         >
                             Search
                         </button>
                     </div>
-                </div>
-                <div className="hidden sm:flex items-center space-x-4">
                     <div className="relative group">
                         <button
                             onClick={() => setIsCategoriesMenuOpen(!isCategoriesMenuOpen)}
-                            className=" flex items-center bg"
+                            className="flex items-center bg"
                         >
                             Categories
                             {isCategoriesMenuOpen ? <FaCaretUp /> : <FaCaretDown />}
@@ -185,39 +186,36 @@ const Header = () => {
                         Cart
                         <span className="bg-s text-white rounded-full px-2 py-1 ml-2">{cartItems.length}</span>
                     </Link>
-
                     {userInfo && (
                         <div className="relative group">
                             {renderProfileButton()}
                         </div>
                     )}
                     {userInfo?.isAdmin && (
-                        <div className="relative group ">
+                        <div className="relative group">
                             {renderAdminButton()}
                         </div>
                     )}
                     {!userInfo && renderSignInButton()}
                 </div>
-                <div className="sm:hidden">
-                    <button
-                        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-                        className="bg text-xl focus:outline-none"
-                    >
-                        ☰
-                    </button>
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        className="p-2 rounded-md border bg w-full mb-2"
-                    />
-                    <button className="bg-[#FF7A1A]  hover:bg-[#FF4A1A] text-white py-2 px-4 rounded-md w-full mb-2">
-                        Search
-                    </button>
-                </div>
+            </div>
+            <div className="sm:hidden mt-4 flex justify-between">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="p-2 rounded-md border flex-1"
+                    value={keyword}
+                    onChange={(e) => setKeyword(e.target.value)}
+                />
+                <button
+                    className="bg-s text-white py-2 px-4 rounded-md ml-2"
+                    onClick={handleSearch}
+                >
+                    Search
+                </button>
             </div>
             {isMobileMenuOpen && (
                 <div className="mt-4 sm:hidden">
-                   
                     <div className="space-y-2">
                         <div className="relative group">
                             <button
@@ -254,12 +252,12 @@ const Header = () => {
                             <span className="bg-s text-white rounded-full px-2 py-1 ml-2">{cartItems.length}</span>
                         </Link>
                         {userInfo && (
-                            <div className="relative group ">
+                            <div className="relative group">
                                 {renderProfileButton()}
                             </div>
                         )}
                         {userInfo?.isAdmin && (
-                            <div className="relative group ">
+                            <div className="relative group">
                                 {renderAdminButton()}
                             </div>
                         )}
